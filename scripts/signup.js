8d8b4c1dbb27bda5cacb0b7a9ae894fd72890f7a
iamjada <jsenebouttarath@mail.greenriver.edu>
2021-06-12 21:13:44 -0400

    Working javascript validation for signup.html and modified styles.css

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,7 +1,10 @@
 document.getElementById("signup").onsubmit = validate;
 function validate() {
-    let Validated = true;
 
+    //flag variable
+    let isValid = true;
+
+    // clear error messages
     let errors = document.getElementsByClassName("err")
     for (let i = 0; i < errors.length; i++) {
         errors[i].style.display = "none";
@@ -12,7 +15,7 @@ function validate() {
     if (firstName === "") {
         let errFirstName = document.getElementById("err-fname");
         errFirstName.style.display = "inline";
-        Validated = false;
+        isValid = false;
     }
 
     // check last
@@ -20,7 +23,7 @@ function validate() {
     if (lName === "") {
         let errLastName = document.getElementById("err-lname");
         errLastName.style.display = "inline";
-        Validated = false;
+        isValid = false;
     }
 
     //check Username
@@ -28,7 +31,7 @@ function validate() {
     if (userName === "") {
         let errUserName = document.getElementById("err-username");
         errUserName.style.display = "inline";
-        Validated = false;
+        isValid = false;
     }
 
     //check Password
@@ -36,7 +39,7 @@ function validate() {
     if (passWord === "") {
         let errPass = document.getElementById("err-password");
         errPass.style.display = "inline";
-        Validated = false;
+        isValid = false;
     }
 
 
@@ -46,8 +49,9 @@ function validate() {
     if (Email === "") {
         let errEmail = document.getElementById("err-email");
         errEmail.style.display = "inline";
-        Validated = false;
+        isValid = false;
     }
 
+    return isValid;
 }
 
